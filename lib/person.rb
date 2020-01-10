fb87e3a78f8f36a1e746221166b7851f5acf94ac
Joanna E K <kalemajoanna@gmail.com>
2020-01-10 10:08:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,10 @@
 class Person
   #your code here
   
-attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(hair_color:, eye_color:)
-    @hair_color = hair_color
-    @eye_color = eye_color
+  def initialize(args)
+   args
   end 
 end
